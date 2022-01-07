0e25fc1e2b8b161d1ad9abe8f99136669ed35532
wpgreene <wpgreene@ncsu.edu>
2022-01-07 05:18:07 -0500

    Setting content disposition header

--- src/index.js
+++ src/index.js
@@ -60,73 +60,79 @@ GLTF_SERVER.get('/', (req, res) => {
                     + CONVERSION_CONSTANTS.RGB_MIN_VALUE + "-" + CONVERSION_CONSTANTS.RGB_MAX_VALUE + ")"
             });
         }
+        else {
+            res.append('Access-Control-Expose-Headers', 'Content-Disposition');
 
-        const region1Color = new RegionColor(region1[0], region1[1], region1[2]);
-        const region2Color = new RegionColor(region2[0], region2[1], region2[2]);
-        const region3Color = new RegionColor(region3[0], region3[1], region3[2]);
-        const region4Color = new RegionColor(region4[0], region4[1], region4[2]);
-        const region5Color = new RegionColor(region5[0], region5[1], region5[2]);
+            const region1Color = new RegionColor(region1[0], region1[1], region1[2]);
+            const region2Color = new RegionColor(region2[0], region2[1], region2[2]);
+            const region3Color = new RegionColor(region3[0], region3[1], region3[2]);
+            const region4Color = new RegionColor(region4[0], region4[1], region4[2]);
+            const region5Color = new RegionColor(region5[0], region5[1], region5[2]);
 
-        const mtlTemplate = `
-            #
-            # Bunkspeed OBJ Material File
-            # http://www.bunkspeed.com
-            #
-            newmtl speaker_mesh
-            Kd ${region1Color.red} ${region1Color.green} ${region1Color.blue}
-            Ks 0.5 0.5 0.5
-            illum 2
-            Ns 99.90109
-            newmtl burnished_titanium
-            Kd 1 0.9607843 0.9294118
-            Ks 1 0.9607843 0.9294118
-            illum 2
-            Ns 6.954887
-            newmtl white_low_gloss_plastic
-            Kd 1 1 1
-            Ks 0.5 0.5 0.5
-            illum 2
-            Ns 1.801619
-            newmtl burnished_brass
-            Kd 1 0.8705882 0.5294118
-            Ks 1 0.8705882 0.5294118
-            illum 2
-            Ns 6.954887
-            newmtl speaker_cover
-            Kd ${region2Color.red} ${region2Color.green} ${region2Color.blue}
-            Ks 0.5 0.5 0.5
-            illum 2
-            Ns 1.614907
-            newmtl speaker_power_indicator
-            Kd ${region3Color.red} ${region3Color.green} ${region3Color.blue}
-            Ks 0.5 0.5 0.5
-            illum 2
-            newmtl speaker_insignia
-            Kd ${region4Color.red} ${region4Color.green} ${region4Color.blue}
-            Ks 0.5 0.5 0.5
-            illum 2
-            Ns 99.90109
-            newmtl black_low_gloss_plastic
-            Kd 0.1019608 0.1019608 0.1019608
-            Ks 0.5 0.5 0.5
-            illum 2
-            Ns 1.801619
-            newmtl speaker_bottom_screw
-            Kd ${region5Color.red} ${region5Color.green} ${region5Color.blue}
-            Ks 0.5 0.5 0.5
-            illum 2
-        `;
+            const mtlTemplate = `
+                #
+                # Bunkspeed OBJ Material File
+                # http://www.bunkspeed.com
+                #
+                newmtl speaker_mesh
+                Kd ${region1Color.red} ${region1Color.green} ${region1Color.blue}
+                Ks 0.5 0.5 0.5
+                illum 2
+                Ns 99.90109
+                newmtl burnished_titanium
+                Kd 1 0.9607843 0.9294118
+                Ks 1 0.9607843 0.9294118
+                illum 2
+                Ns 6.954887
+                newmtl white_low_gloss_plastic
+                Kd 1 1 1
+                Ks 0.5 0.5 0.5
+                illum 2
+                Ns 1.801619
+                newmtl burnished_brass
+                Kd 1 0.8705882 0.5294118
+                Ks 1 0.8705882 0.5294118
+                illum 2
+                Ns 6.954887
+                newmtl speaker_cover
+                Kd ${region2Color.red} ${region2Color.green} ${region2Color.blue}
+                Ks 0.5 0.5 0.5
+                illum 2
+                Ns 1.614907
+                newmtl speaker_power_indicator
+                Kd ${region3Color.red} ${region3Color.green} ${region3Color.blue}
+                Ks 0.5 0.5 0.5
+                illum 2
+                newmtl speaker_insignia
+                Kd ${region4Color.red} ${region4Color.green} ${region4Color.blue}
+                Ks 0.5 0.5 0.5
+                illum 2
+                Ns 99.90109
+                newmtl black_low_gloss_plastic
+                Kd 0.1019608 0.1019608 0.1019608
+                Ks 0.5 0.5 0.5
+                illum 2
+                Ns 1.801619
+                newmtl speaker_bottom_screw
+                Kd ${region5Color.red} ${region5Color.green} ${region5Color.blue}
+                Ks 0.5 0.5 0.5
+                illum 2
+            `;
 
-        fs.writeFileSync(path.join(__dirname, "UE_MEGABOOM.mtl"), mtlTemplate);
+            fs.writeFileSync(path.join(__dirname, "UE_MEGABOOM.mtl"), mtlTemplate);
 
-        obj2gltf(path.join(__dirname, "UE_MEGABOOM.obj")).then((gltf) => {
-            const gltfData = Buffer.from(JSON.stringify(gltf));
-            fs.writeFileSync(path.join(__dirname, "UE_MEGABOOM.gltf"), gltfData);
-        });
+            obj2gltf(path.join(__dirname, "UE_MEGABOOM.obj")).then((gltf) => {
+                const gltfData = Buffer.from(JSON.stringify(gltf));
+                fs.writeFileSync(path.join(__dirname, "UE_MEGABOOM.gltf"), gltfData);
+            }).catch(error => {
+                console.log(error);
+            });
 
-        res.setHeader('Content-Type', 'model/gltf+json');
-        res.status(200);
-        res.sendFile(path.join(__dirname, "UE_MEGABOOM.gltf"));
+            res.setHeader('Content-Type', 'model/gltf+json');
+            res.setHeader('Content-Disposition', 'attachment; filename="UE_MEGABOOM.gltf"');
+            res.status(200);
+            res.sendFile(path.join(__dirname, "UE_MEGABOOM.gltf"));
+        }
     }
 });
 
