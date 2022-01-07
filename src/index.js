ac899edcf7fef4d9f445ec43a31794aef6d0b2a1
wpgreene <wpgreene@ncsu.edu>
2022-01-07 02:42:56 -0500

    Implemented server boilerplate

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const cors = require('cors');
+const express = require('express');
+const fs = require('fs');
+const obj2gltf = require('obj2gltf');
+
+const GLTF_SERVER = express();
+const GLTF_SERVER_PORT = 2330;
+
+GLTF_SERVER.get('/', (req, res) => {
+
+});
+
+GLTF_SERVER.listen(process.env.PORT || GLTF_SERVER_PORT);
